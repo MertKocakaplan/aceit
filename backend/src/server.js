0e1040bae57041fc83e4c1bc301db94dea08a7d9
MertKocakaplan <bm1234-41@hotmail.com>
2025-12-16 18:25:38 +0300

    railway sorunu için güncelleme

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const logger = require('./utils/logger');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   logger.info(`🚀 Server running on port ${PORT}`);
   logger.info(`Environment: ${process.env.NODE_ENV}`);
   logger.info(`Frontend URL: ${process.env.FRONTEND_URL}`);
