840ed3a124b82b70d8121ce66b47e7a416ce47f3
MertKocakaplan <bm1234-41@hotmail.com>
2025-12-16 19:54:53 +0300

    railway sorunu için güncelleme

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
+require('dotenv').config(); // Bunu yorum satırına almanızı şiddetle tavsiye ederim.
+console.log('--- 1. require dotenv geçildi ---'); // Yorum satırı ise, silin.
+
 const app = require('./app');
+console.log('--- 2. app.js başarıyla require edildi ---'); // Eğer bunu görmüyorsak, hata app.js içindedir.
+
 const logger = require('./utils/logger');
+console.log('--- 3. Logger başarıyla require edildi ---'); // Eğer bunu görmüyorsak, hata logger.js içindedir.
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  logger.info(`🚀 Server running on port ${PORT}`);
-  logger.info(`Environment: ${process.env.NODE_ENV}`);
-  logger.info(`Frontend URL: ${process.env.FRONTEND_URL}`);
+  logger.info(`🚀 Server running on port ${PORT}`);
+  console.log('--- 4. app.listen BAŞARILI ---'); // Eğer burayı görmüyorsak, çöküş ondan öncedir.
 });
